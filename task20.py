e4a969293a5a6b6b1a3422746156837bfcb6cb7a
HuGoxDP <sasha.maidanika@gmail.com>
2022-03-08 13:02:36 +0200

    task 20

---
+++ task20.py
@@ -0,0 +1,18 @@
+"""20. Присвоїти цілій змінній d першу цифру з дробової частини
+позитивного дійсного числа x
+"""
+
+
+def assign1(x: float) -> float:
+    temp = int(x)
+    x -= temp
+    d = x * 10
+    return (int(d))
+
+
+x = float(input("x = "))
+
+while x <= 0:
+    x = float(input("Enter x > 0 " + "\n" + "x = "))
+
+print("d = " + (str(assign1(x))))
