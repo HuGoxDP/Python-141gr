8bbc6cddeee042cc49d0b738d7928537267cd790
HuGoxDP <sasha.maidanika@gmail.com>
2022-03-09 12:54:14 +0200

    fix3

--- lab1/task5.py
+++ lab1/task5.py
@@ -4,10 +4,11 @@
 
 def geometric(x, y, z):
     temp = (z * x * y) ** (1/3)
-    return temp
+    
+    return print("Geometric mean = " + str(temp) + "\n" + "Fractional part = " + str(temp - int(temp))  )
 
 
 x = int(input("x = "))
 y = int(input("y = "))
 z = int(input("z = "))
-print(geometric(x, y, z))
+geometric(x, y, z)
