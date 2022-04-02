53e6865a8f137d486ec0e728fcfa766c2e4a22f3
HuGoxDP <sasha.maidanika@gmail.com>
2022-04-02 17:12:32 +0300

    fix

--- lab3/task11.py
+++ lab3/task11.py
@@ -1,6 +1,10 @@
 """11. Дано цілі числа K і N (N> 0). Вивести N раз число K.
 """
 
+def culcul(a, b):
+    for i in range(b):
+        print(a)
+
 
 def check(x: int):
     while True:
@@ -20,8 +24,8 @@ def lop():
 def lopoper():
     a, b = lop()
     b = check(b)
-    for i in range(b):
-        print(a)
+    culcul(a, b)
+
 
 
 lopoper()
